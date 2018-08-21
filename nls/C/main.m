66000f4e2ce05889831d5f8294d40311fd427124
kroelld <dkroell@acm.org>
2018-08-21 11:22:23 -0400

    Update main.m

--- nls/C/main.m
+++ nls/C/main.m
@@ -7,9 +7,8 @@ $ #DISPLAYRequiresArg
 $ #WarnDisplaySet
 # warning: could not set environment variable 'DISPLAY'\n
 $ #Usage
-# Blackbox %s : (c) 2001 - 2002 Sean 'Shaleh' Perry\n\
-  \t\t\t    1997 - 2000, 2002 Brad Hughes\n\n\
-  -display <string>\t\tuse display connection.\n\
+# ZebX %s : (c) 2018 David Kroell\n\
+<string>\t\tuse display connection.\n\
   -rc <string>\t\t\tuse alternate resource file.\n\
   -version\t\t\tdisplay version and exit.\n\
   -help\t\t\t\tdisplay this help text and exit.\n\n
