c3abd49316330f070b850b9c6ff6713d13dce9d8
kroelld <kroelld.96@gmail.com>
2018-10-13 10:27:41 -0400

    ZUVA 53 - Add your own greeter to serve as the ZebX login.

---
+++ zebx-greeter/zebx_greeter.py
@@ -0,0 +1,14 @@
+#==============================================================#
+#                                                              #
+#           Greeter controller that will utilitze the          #
+#       core linux utilities to provide login and session      #
+#       access to the core user file. This file will also      #
+#       serve as the login portion of the ZebX mainframe.      #
+#                                                              #
+#                        Author: David Kroell                  #
+#                       Version: 0.0.1                         #
+#                                                              #
+#==============================================================#
+
+#!/bin/python
+
